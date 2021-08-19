d5bff6d33d0bab56cd98706d1ed4accae0ff645f
wzru <harryshaunwang@outlook.com>
2021-08-19 21:22:33 +0800

    use key to pass 'conda'

---
+++ mole/conda-version.bash
@@ -0,0 +1,10 @@
+set -euo pipefail
+
+here=`cd $(dirname ${BASH_SOURCE[0]}) && pwd`
+. "${here}/../helper/helper.bash"
+
+env=`cat "${1}/env"`
+
+conda=`must_env_val "${env}" 'conda'`
+
+${conda} --version

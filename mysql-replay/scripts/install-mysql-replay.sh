ca142a5fa0fbde311d23cc1481cf617de16c60aa
Liu Cong <innerr@gmail.com>
2021-08-31 01:24:08 +0800

    update br to support index-backup
    Signed-off-by: Liu Cong <innerr@gmail.com>

--- mysql-replay/scripts/install-mysql-replay.sh
+++ mysql-replay/scripts/install-mysql-replay.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 set -xe
 if [ ! -x "$HOME/bin/mysql-replay" ]; then
-    cd "/tmp/my-rep/store"
+    cd "/tmp/test-sim/my-rep/store"
     rpm -q libpcap-devel || sudo yum install -y libpcap-devel
     test -d mysql-replay || git clone https://github.com/zyguan/mysql-replay.git
     cd mysql-replay
--- mysql-replay/scripts/rotate-hook.sh
+++ mysql-replay/scripts/rotate-hook.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 set -xeu
 file_path="${1}"
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "${file_path}" "127.0.0.1:/tmp/my-rep/store"
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "${file_path}" "127.0.0.1:/tmp/test-sim/my-rep/store"
 rm -f "${file_path}"

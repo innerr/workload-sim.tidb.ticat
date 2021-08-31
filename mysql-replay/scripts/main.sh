5cf16699ab473c259c91fccfe7d81afed7447cd5
Liu Cong <innerr@gmail.com>
2021-09-01 01:41:47 +0800

    remove generated files from repo
    Signed-off-by: Liu Cong <innerr@gmail.com>

--- mysql-replay/scripts/main.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-set -xeu
-
-cd "/tmp/test-sim/my-rep/dump"
-sudo tcpdump -w data.%s.pcap -G 60 -Z "${USER}" -z "$(pwd)/rotate-hook.sh" -i "lo" tcp port 4000 &
-echo $! > tcpdump.ppid
-wait
-echo tcpdump stopped
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null *.pcap "127.0.0.1:/tmp/test-sim/my-rep/store"
--- mysql-replay/scripts/rotate-hook.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-set -xeu
-file_path="${1}"
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "${file_path}" "127.0.0.1:/tmp/test-sim/my-rep/store"
-rm -f "${file_path}"
